ec998be2186ebf81fbd413abc91063acb2fc6bc5
i-coder-op <kaleshivam019@gmail.com>
2023-01-30 19:14:53 +0530

    Angular Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reactive-form-app';
+
+  formData = new FormGroup({
+    formEmail : new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(30)
+    ]),
+    formPassword : new FormControl(''),
+    formAddress : new FormControl(''),
+    formCity : new FormControl(''),
+    formState : new FormControl(''),
+    formPin : new FormControl(''),
+    formCheckMeOut : new FormControl(false)
+  });
+
+  onSubmit(){
+    console.log('Form Submitted');
+    console.log('Email: ' + this.formData.value.formEmail);
+    console.log('Password: ' + this.formData.value.formPassword);
+    console.log('Address: ' + this.formData.value.formAddress);
+    console.log('City: ' + this.formData.value.formCity);
+    console.log('State: ' + this.formData.value.formState);
+    console.log('Pin: ' + this.formData.value.formPin);
+    console.log('Check Me Out: ' + this.formData.value.formCheckMeOut);
+  }
+
 }
